fcfded481f63e4a1a94dcfacc8db1812ff49612d
Doina Fitchevici <domna16@gmail.com>
2024-03-29 20:27:24 -0400

    added clipped drawer layout

--- src/components/Messages/index.jsx
+++ src/components/Messages/index.jsx
@@ -1,12 +1,14 @@
 import styles from "./Messages.module.css";
+import Layout from "./Layout";
 
 const Messages = () => {
   return (
     <div>
-      <h1>Messages</h1>
+      {/* <h1>Messages</h1>
       <p>
         This is the page where users can view and send messages to other users.
-      </p>
+      </p> */}
+      <Layout />
     </div>
   );
 };
