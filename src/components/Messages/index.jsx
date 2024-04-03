709a4b70f67620ce6c84ba27ddb80e925e05eabc
Doina Fitchevici <domna16@gmail.com>
2024-04-03 19:49:31 -0400

    Removed module.css

--- src/components/Messages/index.jsx
+++ src/components/Messages/index.jsx
@@ -1,14 +1,5 @@
-import styles from "./Messages.module.css";
-
 const Messages = () => {
-  return (
-    <div>
-      <h1>Messages</h1>
-      <p>
-        This is the page where users can view and send messages to other users.
-      </p>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Messages;
