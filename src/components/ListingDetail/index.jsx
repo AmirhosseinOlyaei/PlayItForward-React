fb016a2fdcfe10efaf3bd059edc050a1a2127e90
Alevtina-Babinskaya <letters4alka@gmail.com>
2024-04-07 19:12:19 -0700

    personal info updates, toy details update

--- src/components/ListingDetail/index.jsx
+++ src/components/ListingDetail/index.jsx
@@ -1,5 +1,6 @@
+import React from "react";
 import styles from "./ListingDetail.module.css";
-import { Button, ButtonGroup, Typography, Box, Divider } from '@mui/material';
+import { Button, ButtonGroup, Typography, Box, Divider, Icon, Avatar } from '@mui/material';
 import { Input } from '@mui/material';
 import { TextField } from "@mui/material";
 import Grid from '@mui/material/Unstable_Grid2';
@@ -10,43 +11,83 @@ import MailIcon from '@mui/icons-material/Mail';
 import Bookmark from '@mui/icons-material/Bookmark';
 import ShareIcon from '@mui/icons-material/Share';
 import ActionButton from "../UserProfile/ActionButton";
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import LocalShippingOutlinedIcon from '@mui/icons-material/LocalShippingOutlined';
+import { useState } from "react";
 //import GoogleMaps from "../ToyList/GoogleMaps";
 
 
 const drawerWidth = 340;
 
 const ListingDetail = () => {
-  const toyLising = {
-    user_id: 1,
-    given_to_user_id: 2,
-    title: "Little Lego cars",
-    description: "5 items, multiple colors, size about 2 inches  each, lego original",
-    condition: "Like new",
-    delivery_method: ["Pickup", "Dropoff"], 
-    pictures: "https://geekculture.co/wp-content/uploads/2020/05/tigermiyaw-8-1200x817.jpg",
-    category: "Cars",
-    zip_code: 94040,
-   
-    created_by_id: 1,
-    create_date: "2024-11-03", 
-    modified_date: "2024-11-03",
-    modified_by_id: 1,
-  };
-  const toyGiver = {
-    email: "jH0H0@example.com",
-    first_name: "Jen",
-    last_name: "Hill",
-    profile_picture: "https://cdn-icons-png.flaticon.com/512/149/149071.png",
-    nickname: "Jennickname",
-    zipcode: 94040,
-    created_by_id: 1,
-    create_date: "March 2022",
-    modified_date: "March 2022",
-    modified_by_id: 1,
 
-  };
+
+  const [toyListing, setToyListing] = useState([]);
+  const [toyGiver, setToyGiver] = useState([]);
+  const [favorites, setFavorites] = useState({});
+  const [isFavorite, setIsFavorite] = useState(false);
+  const [newMessage, setNewMessage] = useState("");
+  const [messageSent, setMessageSent] = useState([]);
+
+  React.useEffect(() => {
+    async function fetchToy(toyId) {
+      const response = await fetch(`http://localhost:8000/api/v1/toys/${toyId}`);
+      const toy = await response.json();
+      setToyListing(toy);
+      fetchToyGiver(toy.listed_by_id);
+    }
+    async function fetchToyGiver(userId) {
+      const response = await fetch(`http://localhost:8000/api/v1/users/${userId}`);
+      const user = await response.json();
+      setToyGiver(user);
+    }
+    fetchToy("660c4de20dab29b8bab994f9"); // Replace with the ID of the toy you want to fetch
+  }, []);
+  
+  function calculateDate(date) {
+    const today = new Date();
+    const todayDate = Date.parse(today);
+    const toyDate = Date.parse(date);
+    const daysAgo = Math.round((todayDate - toyDate) / (1000 * 60 * 60 * 24));
+    return daysAgo
+    
+  }
+
+  const handleFavorite = () => {
+    setIsFavorite(!isFavorite);
+    
+    console.log(toyListing._id);
+    const fav = {
+      toy_listing_id: toyListing._id,
+      user_id: "6609a2873eaffef95345b9fa", // Replace with the ID of the user who is logged in
+    };
+    addFavorite(fav);
+  }
+
+  async function addFavorite(fav) {
+    const response = await fetch("http://localhost:8000/api/v1/favorites", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(fav),
+    });
+
+    setFavorites(await response.json());
+  }
+
+  async function addFavorite(fav) {
+    const response = await fetch(`http://localhost:8000/api/v1/favorites/${fav._id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    setFavorites({});
+  }
+  
   return (
-
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
       <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
@@ -64,13 +105,18 @@ const ListingDetail = () => {
         <Box sx={{ overflow: 'auto', padding: "0px 20px" }}>
           
             <Box sx={{ padding: "20px 0" }}>
-            <Typography variant="h4" sx={{ margin: "5px 0" }}>{toyLising.title}</Typography>
-            <Typography variant="body">Listed {toyLising.create_date} in {toyLising.zip_code} </Typography>
-            <Typography variant="body"><span>{toyLising.delivery_method[0]}, {toyLising.delivery_method[1]}</span></Typography>
+            <Typography variant="h4" sx={{ margin: "5px 0" }}>{toyListing.title}</Typography>
+            
+              <Typography variant="body" paragraph>Listed {calculateDate(toyListing.create_date)} days ago in {toyListing.zip_code} </Typography>
+            
+            <Box sx={{ display: "flex", justifyContent: "space-between", maxWidth: "80%"}}>
+              <Typography variant="body2" sx={{ display: "flex", alignItems: "center" }}><HomeOutlinedIcon sx={{ fontSize: 32 }}/><b style={{ marginLeft: "10px" }}>{toyListing.delivery_method}</b></Typography>
+              <Typography variant="body2" sx={{ display: "flex", alignItems: "center" }}><LocalShippingOutlinedIcon sx={{ fontSize: 32 }}/><b style={{  marginLeft: "10px" }}>{toyListing.delivery_method}</b></Typography>
+            </Box>
             <Grid xs={12} sx={{ margin: "10px 0", display: "flex", justifyContent: "space-between" }}>
-              <ActionButton linkTo="/messages" text="Message" startIcon={<MailIcon/>}/>
-              <ActionButton linkTo="" text="" startIcon={<Bookmark/>}/>
-              <ActionButton linkTo="" text="" startIcon={<ShareIcon/>} />
+              <ActionButton linkTo="/messages" text="Message" startIcon={<MailIcon/>} fullWidth={false}/>
+              <ActionButton linkTo="" text="" startIcon={<Bookmark/>} fullWidth={false} onClick={handleFavorite}/>
+              <ActionButton linkTo="" text="" startIcon={<ShareIcon/>} fullWidth={false}/>
             </Grid>
             </Box>
             <Divider/>
@@ -78,43 +124,50 @@ const ListingDetail = () => {
               <Typography variant="h6" sx={{ margin: "5px 0" }}>Details</Typography>
                 <div className={styles.detailsRow}>
                   <div className={styles.detailsLabel}><Typography variant="body"><b>Category</b></Typography></div>
-                  <div><Typography variant="body">{toyLising.category}</Typography></div>
+                  <div><Typography variant="body">{toyListing.category}</Typography></div>
                 </div>
                 <div className={styles.detailsRow}>
                   <div className={styles.detailsLabel}><Typography variant="body"><b>Condition</b></Typography></div>
-                  <div><Typography variant="body">{toyLising.condition}</Typography></div>
+                  <div><Typography variant="body">{toyListing.condition}</Typography></div>
                 </div>
                 <div className={styles.detailsRow}>
                   <div className={styles.detailsLabel}><Typography variant="body"><b>Description</b></Typography></div>  
-                  <div><Typography variant="body">{toyLising.description}</Typography></div>
+                  <div><Typography variant="body">{toyListing.description}</Typography></div>
                 </div>
             </Box>
             <Divider/>
             <Box sx={{ padding: "20px 0" }}>
               <Typography variant="h6" sx={{ margin: "5px 0" }}>Toy giver information</Typography>
-                <div className={styles.giverInformation}>
-                  <img src={toyGiver.profile_picture} alt="Toy giver profile picture" width="42px" height="42px" />
+                <Box className={styles.giverInformation}>
+                  <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="Toy giver profile picture" width="42px" height="42px" />
                   <Typography variant="body" sx={{ marginLeft: "10px", lineHeight: "42px" }}>{toyGiver.nickname}</Typography>
-                </div>
-                <Typography variant="body">Joined <b>PlayItForward</b> in {toyGiver.create_date}</Typography>
+                </Box>
+                <Box sx={{ marginTop: "10px" }}>
+                  <Typography variant="body">
+                    <Avatar src="/AppLogo.png" alt="AppLogo" sx={{ width: 30, height: 30, display: "inline-block", verticalAlign: "middle" }}/>
+                     Joined <b>PlayItForward</b> in {dateStringToMonthYear(toyGiver.create_date)}
+                  </Typography>
+                </Box>
             </Box>
             <Divider/>
             <Box sx={{ padding: "20px 0" }}>
               <Typography variant="h6" sx={{ margin: "5px 0" }}>Send a message</Typography>
               <TextField id="outlined-basic" label="Is this still available?" variant="outlined" sx={{ width: "100%" }} />
               <br/>
-              <ActionButton linkTo="" text="Send" startIcon={<MailIcon/>} /> 
+              <ActionButton linkTo="" text="Send" startIcon={<MailIcon/>} fullWidth={true}/> 
             </Box>
-          
         </Box>
       </Drawer>
       <Box component="main" sx={{ flexGrow: 1, p: 3,  mt: 12 }}>
-        <img src={toyLising.pictures} alt="Toy image" width="100%" />
+        <img src="https://geekculture.co/wp-content/uploads/2020/05/tigermiyaw-8-1200x817.jpg" alt="Toy image" width="100%" />
       </Box>
     </Box>
-
-
   );
 };
+
+export function dateStringToMonthYear(dateString) {
+  if (!dateString) return "";
+  return new Date(dateString).toLocaleDateString('en-US', { month: 'long', year: 'numeric' });
+}
 
 export default ListingDetail;
--- src/components/UserProfile/PersonalInfo.jsx
+++ src/components/UserProfile/PersonalInfo.jsx
@@ -6,22 +6,92 @@ import { Avatar, Button, Divider, Typography, IconButton, Box } from "@mui/mater
 import ImgMediaCard from "./oneLising";
 import CssBaseline from '@mui/material/CssBaseline';
 import AppBar from '@mui/material/AppBar';
+import React from "react";
+import { useState, useEffect } from "react";
+import { dateStringToMonthYear } from "../ListingDetail";
+import DoneIcon from '@mui/icons-material/Done';
+
 
 
 const PersonalInfo = () => {
-  const user = {
-    email: "jH0H0@example.com",
-    first_name: "Jen",
-    last_name: "Hill",
-    profile_picture: "https://cdn-icons-png.flaticon.com/512/149/149071.png",
-    nickname: "Jennickname",
-    zipcode: 94040,
-    created_by_id: 1,
-    create_date: "March 2022",
-    modified_date: "March 2022",
-    modified_by_id: 1,
+  // const user = {
+  //   email: "jH0H0@example.com",
+  //   first_name: "Jen",
+  //   last_name: "Hill",
+  //   profile_picture: "https://cdn-icons-png.flaticon.com/512/149/149071.png",
+  //   nickname: "Jennickname",
+  //   zipcode: 94040,
+  //   created_by_id: 1,
+  //   create_date: "March 2022",
+  //   modified_date: "March 2022",
+  //   modified_by_id: 1,
+  // };
 
+  const [userSignedIn, setUserSignedIn] = React.useState({});
+  const [editNickNameMode, setEditNickNameMode] = React.useState(false);
+  const [newNickname, setNewNickname] = React.useState(userSignedIn.nickname);
+  const [newZipcode, setNewZipcode] = React.useState(userSignedIn.zipcode);
+  const [editZipcodeMode, setEditZipcodeMode] = React.useState(false);
+  const currentUserId = "6609a2873eaffef95345b9f9"; // Replace with the actual user ID of the currently logged-in user
+
+  React.useEffect(() => {
+    async function fetchUser(userId) {
+      const response = await fetch(`http://localhost:8000/api/v1/users/${userId}`);
+      const user = await response.json();
+      setUserSignedIn(user);
+      setNewNickname(user.nickname);
+      setNewZipcode(user.zipcode);
+    }
+    fetchUser("6609a2873eaffef95345b9f9");
+  }, []);
+
+  const handleEditNickName = () => {
+    setEditNickNameMode(true);
   };
+
+  const handleSaveNickName = () => {
+    setEditNickNameMode(false);
+    updateNickname(newNickname);
+  }
+
+  const handleEditZipCode = () => {
+    setEditZipcodeMode(true);
+  };
+
+  const handleSaveZipCode = () => {
+    setEditZipcodeMode(false);
+    updateZipcode(newZipcode);
+  }
+
+async function updateNickname (newNickname) {
+  const response = await fetch(`http://localhost:8000/api/v1/users/${currentUserId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      nickname: newNickname,
+    }),
+  });
+  const updatedUser = await response.json();
+  setUserSignedIn(updatedUser);
+}
+
+async function updateZipcode (newZipcode) {
+  console.log(newZipcode, userSignedIn.id);
+  const response = await fetch(`http://localhost:8000/api/v1/users/${currentUserId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      zipcode: newZipcode,
+    }),
+  });
+  const updatedUser = await response.json();
+  setUserSignedIn(updatedUser);
+}
+  
   return (
 
     <Box sx={{ display: 'flex' }}>
@@ -34,24 +104,35 @@ const PersonalInfo = () => {
        
     <div className={styles.userProfile}>
       <div>
-        <p><Typography variant="h3">{user.first_name} {user.last_name}</Typography></p>
-        <p><Typography variant="body">{user.nickname}   
-          <IconButton aria-label="edit" size="small">
-            <EditIcon />
-          </IconButton>
+        <Typography variant="h3">{userSignedIn.first_name}  {userSignedIn.last_name}</Typography>
+        <p><Typography variant="body">Joined <b>PlayItForward</b> in {dateStringToMonthYear(userSignedIn.create_date)}</Typography></p>
+        <p><Typography variant="body">Nickname: 
+        {editNickNameMode ? <input type="text" value={newNickname} onChange={(e) => setNewNickname(e.target.value)} /> : userSignedIn.nickname} &nbsp; &nbsp;
+          {editNickNameMode ? 
+            <IconButton aria-label="save" size="small" onClick={handleSaveNickName}>
+              <DoneIcon/>
+            </IconButton> 
+            : 
+            <IconButton aria-label="edit" size="small" onClick={handleEditNickName}>
+              <EditIcon />
+            </IconButton>}
         </Typography> 
         </p>
         
-        <p><Typography variant="body">Joined {user.create_date}</Typography></p>
-        <p><Typography variant="body">{user.email}</Typography></p>
-        <p><Typography variant="body">Location: {user.zipcode} 
-          <IconButton aria-label="edit" size="small">
+        
+        <p><Typography variant="body">E-mail: {userSignedIn.email}</Typography></p>
+        <p><Typography variant="body">Location: 
+        {editZipcodeMode ? <input type="text" value={newZipcode} onChange={(e) => setNewZipcode(e.target.value)} /> : userSignedIn.zipcode} &nbsp; &nbsp;
+          {editZipcodeMode ? 
+          <IconButton aria-label="save" size="small" onClick={handleSaveZipCode}> <DoneIcon/> </IconButton>
+           : 
+           <IconButton aria-label="edit" size="small" onClick={handleEditZipCode}>
             <EditIcon />
-          </IconButton>
+          </IconButton>}
         </Typography></p>
       </div>
       <div className={styles.avatar}>
-        <Avatar src={user.profile_picture} variant="rounded" sx={{ width: 150, height: 150 }} alt="profile picture" />
+        <Avatar src="https://cdn-icons-png.flaticon.com/512/149/149071.png" variant="rounded" sx={{ width: 150, height: 150 }} alt="profile picture" />
       </div>
     </div>
   </Box>
