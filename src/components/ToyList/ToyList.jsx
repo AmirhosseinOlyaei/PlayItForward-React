70338d5181340697846506fb174d4130c93ef010
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2024-04-14 03:28:10 -0400

    "Switch from using useSelector and map over state.toys to using useState and fetching toys from an API"

--- src/components/ToyList/ToyList.jsx
+++ src/components/ToyList/ToyList.jsx
@@ -1,17 +1,46 @@
+import React, { useState, useEffect } from "react";
 import { Grid } from "@mui/material";
 import ToyCard from "./ToyCard";
 
 function ToyList() {
-  const toys = useSelector((state) => state.toys);
+  const [toys, setToys] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const apiUrl = import.meta.env.VITE_API_URL;
+    const fetchToys = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+          throw new Error("Failed to fetch");
+        }
+        const data = await response.json();
+        setToys(data);
+      } catch (error) {
+        setError(
+          `Error: ${error.response ? error.response.data.message : error.message}`
+        );
+      }
+      setIsLoading(false);
+    };
+
+    fetchToys();
+  }, []); // Empty dependency array means this effect will only run once, similar to componentDidMount()
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
   return (
     <Grid container spacing={{ xs: 2, md: 3 }}>
       {toys.map((toy) => (
-        <Grid item xs={12} sm={6} md={4} key={toy.id}>
+        <Grid item xs={12} sm={6} md={4} key={toy._id}>
           <ToyCard
             title={toy.title}
-            image={toy.image}
-            location={toy.location}
+            images={toy.pictures}
+            location={toy.zip_code}
           />
         </Grid>
       ))}
