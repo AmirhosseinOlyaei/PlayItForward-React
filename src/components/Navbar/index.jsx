13c39a6b4491c375d2c9c8d3c3f28b42be5f93a7
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2024-04-17 04:34:36 -0400

    displaying navigation links has been removed

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,36 +1,8 @@
-import { Link } from "react-router-dom";
-import styles from "./Navbar.module.css";
+// src/components/Navbar/index.jsx
 import NavBar from "./NavBar";
 
 const Navbar = () => {
-  return (
-    <>
-      <nav className={styles.navbar}>
-        <Link className={styles.link} to="/">
-          Home
-        </Link>
-        <Link className={styles.link} to="/toys">
-          Toys
-        </Link>
-        <Link className={styles.link} to="/toy-details">
-          Toy Details
-        </Link>
-        <Link className={styles.link} to="/create">
-          Create Listing
-        </Link>
-        <Link className={styles.link} to="/messages">
-          Messages
-        </Link>
-        <Link className={styles.link} to="/profile">
-          Profile
-        </Link>
-        <Link className={styles.link} to="/login">
-          Login
-        </Link>
-      </nav>
-      <NavBar />
-    </>
-  );
+  return <NavBar />;
 };
 
 export default Navbar;
