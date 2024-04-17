ae4ce3deb661142fcd779a151f1296fd73e69116
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2024-04-16 20:47:25 -0400

    Comment out useState and useEffect hooks for message state and API call in App component
    The diff shows the removal of import statements for `useState` and `useEffect` hooks along with the commenting out of the `message` state and

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import AppRoutes from "./routes/AppRoutes";
 
-import { getAllData } from "./util/index";
+// import { getAllData } from "./util/index";
 
-const URL = "http://localhost:8000/api/v1/";
+// const URL = "http://localhost:8000/api/v1/";
 
 function App() {
-  const [message, setMessage] = useState("");
+  // const [message, setMessage] = useState("");
 
-  useEffect(() => {
-    // (async () => {
-    //   const myData = await getAllData(URL);
-    //   setMessage(myData.data);
-    // })();
+  // useEffect(() => {
+  // (async () => {
+  //   const myData = await getAllData(URL);
+  //   setMessage(myData.data);
+  // })();
 
-    // return () => {
-    //   console.log("unmounting");
-    // };
-  }, []);
+  // return () => {
+  //   console.log("unmounting");
+  // };
+  // }, []);
 
   return (
     <>
       <Router>
         <Navbar />
         <AppRoutes />
-        <p>{message}</p>
+        {/* <p>{message}</p> */}
       </Router>
     </>
   );
