fcbd5be5fb4d71c1b07a023036c9d2d2a543534c
Alevtina-Babinskaya <letters4alka@gmail.com>
2024-04-21 18:52:38 -0700

    rout changes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -15,7 +15,7 @@ const AppRoutes = () => {
       <Route exact path="/" Component={ToyList} />
       <Route path="/toys" Component={ToyList} />
       <Route path="/toys/:id" Component={ListingDetail} />
-      <Route path="/toy-details" Component={ListingDetail} />
+      {/* <Route path="/toy-details" Component={ListingDetail} /> */}
       <Route path="/login" Component={LoginPage} />
       <Route path="/listings" Component={MyListings} />
       <Route path="/messages" Component={Messages} />
