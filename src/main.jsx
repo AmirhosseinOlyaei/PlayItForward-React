d6e56c08fbcd9981448b9be80f881fe350221b07
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2024-04-23 18:12:17 -0400

    Remove React.StrictMode and StyledEngineProvider wrapper
    The diff removes the usage of React.StrictMode and the StyledEngineProvider wrapper around the App component in the main JSX file.

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,7 @@ import "./index.css";
 import { StyledEngineProvider } from "@mui/material/styles";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <StyledEngineProvider injectFirst>
-      <App />
-    </StyledEngineProvider>
-  </React.StrictMode>
+  <StyledEngineProvider injectFirst>
+    <App />
+  </StyledEngineProvider>
 );
