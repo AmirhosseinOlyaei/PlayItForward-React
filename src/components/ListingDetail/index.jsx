77c27ed759f9d756abda2c006a285a6ec9e6edaa
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2024-04-23 20:15:39 -0400

    "Update ListingDetail component with useContext and other changes"

--- src/components/ListingDetail/index.jsx
+++ src/components/ListingDetail/index.jsx
@@ -1,34 +1,40 @@
-import React, { useEffect} from "react";
-import { useNavigate } from 'react-router-dom';
+import React, { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./ListingDetail.module.css";
-import { Typography, Box, Divider, Avatar, Popover, TextField } from '@mui/material';
-import Grid from '@mui/material/Unstable_Grid2';
-import CssBaseline from '@mui/material/CssBaseline';
-import Drawer from '@mui/material/Drawer';
-import AppBar from '@mui/material/AppBar';
-import MailIcon from '@mui/icons-material/Mail';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
+import {
+  Typography,
+  Box,
+  Divider,
+  Avatar,
+  Popover,
+  TextField,
+} from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2";
+import CssBaseline from "@mui/material/CssBaseline";
+import Drawer from "@mui/material/Drawer";
+import AppBar from "@mui/material/AppBar";
+import MailIcon from "@mui/icons-material/Mail";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
 import ActionButton from "../UserProfile/ActionButton";
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import LocalShippingOutlinedIcon from '@mui/icons-material/LocalShippingOutlined';
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import LocalShippingOutlinedIcon from "@mui/icons-material/LocalShippingOutlined";
 import { useState } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import ToyMap from "./ToyMap";
 import { useParams } from "react-router-dom";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import UserContext from "../../context/userContext";
 
-
-
-const apiUrl = import.meta.env.VITE_API_URL
+const apiUrl = import.meta.env.VITE_API_URL;
 const GOOGLE_MAPS_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 const drawerWidth = 340;
 // const toyListingId = "66196e990925b15c9b3c4375"; // Replace with the ID of the toy listing, which comes from the URL
-const authorizedUser = "6609a2873eaffef95345b9fa"; // Replace with the ID of the user who is logged in
 
 const ListingDetail = () => {
-
+  const user = useContext(UserContext);
+  const authorizedUser = user._id;
   const { id } = useParams();
   const [toyListing, setToyListing] = useState({});
   const [toyGiver, setToyGiver] = useState([]);
@@ -49,22 +55,23 @@ const ListingDetail = () => {
       const user = await response.json();
       setToyGiver(user);
     }
-    async function checkFavorite (userId, toyId) {
-      const response = await fetch(`${apiUrl}/favorites/check-favorite/${userId}/${toyId}`);
+    async function checkFavorite(userId, toyId) {
+      const response = await fetch(
+        `${apiUrl}/favorites/check-favorite/${userId}/${toyId}`
+      );
       const favoriteCheck = await response.json();
       setIsFavorite(favoriteCheck);
       console.log(favoriteCheck);
     }
     fetchToy(id); // Replace with the ID of the toy you want to fetch
   }, []);
-  
+
   function calculateDate(date) {
     const today = new Date();
     const todayDate = Date.parse(today);
     const toyDate = Date.parse(date);
     const daysAgo = Math.round((todayDate - toyDate) / (1000 * 60 * 60 * 24));
-    return daysAgo
-    
+    return daysAgo;
   }
 
   const handleFavorite = () => {
@@ -74,7 +81,7 @@ const ListingDetail = () => {
     };
     isFavorite ? deleteFavorite(fav) : addFavorite(fav);
     setIsFavorite(!isFavorite);
-  }
+  };
 
   async function addFavorite(fav) {
     const response = await fetch(`${apiUrl}/favorites`, {
@@ -84,7 +91,7 @@ const ListingDetail = () => {
       },
       body: JSON.stringify(fav),
     });
-    console.log("The toy is added", toyListing._id );
+    console.log("The toy is added", toyListing._id);
   }
 
   async function deleteFavorite(fav) {
@@ -94,14 +101,14 @@ const ListingDetail = () => {
         "Content-Type": "application/json",
       },
     });
-    console.log("Deleted from favorites", toyListing._id );
-  };
+    console.log("Deleted from favorites", toyListing._id);
+  }
 
   const handleMessageChange = (event) => {
     setNewMessage(event.target.value);
   };
   const handleSendMessage = async () => {
-    console.log(id, toyListing.listed_by_id._id)
+    console.log(id, toyListing.listed_by_id._id);
     try {
       const response = await fetch(`${apiUrl}/messages`, {
         method: "POST",
@@ -140,120 +147,248 @@ const ListingDetail = () => {
   const navigate = useNavigate();
 
   const handleBack = () => {
-      navigate(-1);  // Navigate back to the previous page
+    navigate(-1); // Navigate back to the previous page
   };
-  const [mapPosition, setMapPosition] = useState({ lat: null, lng: null, city: "", state: "" });
+  const [mapPosition, setMapPosition] = useState({
+    lat: null,
+    lng: null,
+    city: "",
+    state: "",
+  });
   useEffect(() => {
     if (!toyListing.zip_code) {
-      return
+      return;
     }
-    fetch(`https://maps.googleapis.com/maps/api/geocode/json?components=postal_code:${toyListing.zip_code}|country:us&key=${GOOGLE_MAPS_API_KEY}`)
-      .then(response => response.json())
-      .then(data => {
-
+    fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?components=postal_code:${toyListing.zip_code}|country:us&key=${GOOGLE_MAPS_API_KEY}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
         const location = data.results[0].geometry.location;
         const city = data.results[0].address_components[1].long_name;
         const state = data.results[0].address_components[2].long_name;
         const latitude = location.lat;
         const longitude = location.lng;
-        setMapPosition({ lat: latitude, lng: longitude, city: city, state: state });
+        setMapPosition({
+          lat: latitude,
+          lng: longitude,
+          city: city,
+          state: state,
+        });
         console.log("mapPosition", mapPosition);
-    
       })
-      .catch(error => console.error("Error:", error));
+      .catch((error) => console.error("Error:", error));
   }, [toyListing]);
-  
+
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
-      <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-        
-      </AppBar>
+      <AppBar
+        position="fixed"
+        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      ></AppBar>
       <Drawer
         variant="permanent"
         sx={{
           width: drawerWidth,
           flexShrink: 0,
-          [`& .MuiDrawer-paper`]: { width: drawerWidth, BoxSizing: 'border-Box', marginTop: "86px", height: "calc(100vh - 90px)", },
+          [`& .MuiDrawer-paper`]: {
+            width: drawerWidth,
+            BoxSizing: "border-Box",
+            marginTop: "86px",
+            height: "calc(100vh - 90px)",
+          },
         }}
       >
-        
-        <Box sx={{ overflow: 'auto', padding: "0px 20px" }}>
-          <ActionButton link=""  text="&nbsp;Back" startIcon={<ArrowBackIcon/>} onClick={handleBack} fullWidth={false}/>
+        <Box sx={{ overflow: "auto", padding: "0px 20px" }}>
+          <ActionButton
+            link=""
+            text="&nbsp;Back"
+            startIcon={<ArrowBackIcon />}
+            onClick={handleBack}
+            fullWidth={false}
+          />
           <Box sx={{ padding: "20px 0" }}>
-            <Typography variant="h4" sx={{ margin: "5px 0" }}>{toyListing.title}</Typography>
-            <Typography variant="body" paragraph>Listed {calculateDate(toyListing.created_date)} days ago in {mapPosition.city}, {mapPosition.state} </Typography>
-            <Box sx={{ display: "flex", justifyContent: "space-between", maxWidth: "80%"}}>
-              <Typography variant="body2" sx={{ display: "flex", alignItems: "center" }}>
-                {toyListing.delivery_method === "Delivery" ? (<LocalShippingOutlinedIcon sx={{ fontSize: 32 }}/>) : 
-                (<HomeOutlinedIcon sx={{ fontSize: 32 }}/>)
-                }
-                <b style={{ marginLeft: "10px" }}>{toyListing.delivery_method}</b>
+            <Typography variant="h4" sx={{ margin: "5px 0" }}>
+              {toyListing.title}
+            </Typography>
+            <Typography variant="body" paragraph>
+              Listed {calculateDate(toyListing.created_date)} days ago in{" "}
+              {mapPosition.city}, {mapPosition.state}{" "}
+            </Typography>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                maxWidth: "80%",
+              }}
+            >
+              <Typography
+                variant="body2"
+                sx={{ display: "flex", alignItems: "center" }}
+              >
+                {toyListing.delivery_method === "Delivery" ? (
+                  <LocalShippingOutlinedIcon sx={{ fontSize: 32 }} />
+                ) : (
+                  <HomeOutlinedIcon sx={{ fontSize: 32 }} />
+                )}
+                <b style={{ marginLeft: "10px" }}>
+                  {toyListing.delivery_method}
+                </b>
               </Typography>
             </Box>
-            <Grid xs={12} sx={{ margin: "10px 0", display: "flex", justifyContent: "space-between" }}>
-              <ActionButton link={`/messages?id=${id}`}  text="&nbsp;Message" startIcon={<MailIcon/>} fullWidth={false}/>
-              <ActionButton link="" text="" startIcon={isFavorite ? <FavoriteIcon/> : <FavoriteBorderIcon/>} fullWidth={false} onClick={handleFavorite}/>
-              <CopyToClipboard text={`${apiUrl}/toys/${id}`} onCopy={() => setIsOpen(true)}> 
-                <ActionButton text="" startIcon={<ShareIcon/>} fullWidth={false} onClick={(event) => setAnchorEl(event.currentTarget)}/>
+            <Grid
+              xs={12}
+              sx={{
+                margin: "10px 0",
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              <ActionButton
+                link={`/messages?id=${id}`}
+                text="&nbsp;Message"
+                startIcon={<MailIcon />}
+                fullWidth={false}
+              />
+              <ActionButton
+                link=""
+                text=""
+                startIcon={
+                  isFavorite ? <FavoriteIcon /> : <FavoriteBorderIcon />
+                }
+                fullWidth={false}
+                onClick={handleFavorite}
+              />
+              <CopyToClipboard
+                text={`${apiUrl}/toys/${id}`}
+                onCopy={() => setIsOpen(true)}
+              >
+                <ActionButton
+                  text=""
+                  startIcon={<ShareIcon />}
+                  fullWidth={false}
+                  onClick={(event) => setAnchorEl(event.currentTarget)}
+                />
               </CopyToClipboard>
               <Popover
-                  open={isOpen}
-                  anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'left',
-                  }}
-                  anchorEl = {anchorEl}
-                  > 
-                <Typography sx={{ p: 2 }}>The link is copied to clipboard.</Typography>
+                open={isOpen}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                anchorEl={anchorEl}
+              >
+                <Typography sx={{ p: 2 }}>
+                  The link is copied to clipboard.
+                </Typography>
               </Popover>
             </Grid>
+          </Box>
+          <Divider />
+          <Box sx={{ padding: "20px 0" }}>
+            <Typography variant="h6" sx={{ margin: "5px 0" }}>
+              Details
+            </Typography>
+            <div className={styles.detailsRow}>
+              <div className={styles.detailsLabel}>
+                <Typography variant="body">
+                  <b>Category</b>
+                </Typography>
+              </div>
+              <div>
+                <Typography variant="body">{toyListing.category}</Typography>
+              </div>
+            </div>
+            <div className={styles.detailsRow}>
+              <div className={styles.detailsLabel}>
+                <Typography variant="body">
+                  <b>Condition</b>
+                </Typography>
+              </div>
+              <div>
+                <Typography variant="body">{toyListing.condition}</Typography>
+              </div>
+            </div>
+            <div className={styles.detailsRow}>
+              <div className={styles.detailsLabel}>
+                <Typography variant="body">
+                  <b>Description</b>
+                </Typography>
+              </div>
+              <div>
+                <Typography variant="body">{toyListing.description}</Typography>
+              </div>
+            </div>
+          </Box>
+          <Box sx={{ padding: "20px 0" }}>
+            {mapPosition.lat && (
+              <ToyMap lat={mapPosition.lat} lng={mapPosition.lng} />
+            )}
+            <Typography variant="body">
+              {mapPosition.city}, {mapPosition.state}
+            </Typography>
+          </Box>
+          <Divider />
+          <Box sx={{ padding: "20px 0" }}>
+            <Typography variant="h6" sx={{ margin: "5px 0" }}>
+              Posted by
+            </Typography>
+            <Box className={styles.giverInformation}>
+              <img
+                src={toyGiver.profile_picture}
+                alt="Toy giver profile picture"
+                width="42px"
+                height="42px"
+              />
+              <Typography
+                variant="body"
+                sx={{ marginLeft: "10px", lineHeight: "42px" }}
+              >
+                {toyGiver.nickname}
+              </Typography>
             </Box>
-            <Divider/>
-            <Box sx={{ padding: "20px 0" }}>
-              <Typography variant="h6" sx={{ margin: "5px 0" }}>Details</Typography>
-                <div className={styles.detailsRow}>
-                  <div className={styles.detailsLabel}><Typography variant="body"><b>Category</b></Typography></div>
-                  <div><Typography variant="body">{toyListing.category}</Typography></div>
-                </div>
-                <div className={styles.detailsRow}>
-                  <div className={styles.detailsLabel}><Typography variant="body"><b>Condition</b></Typography></div>
-                  <div><Typography variant="body">{toyListing.condition}</Typography></div>
-                </div>
-                <div className={styles.detailsRow}>
-                  <div className={styles.detailsLabel}><Typography variant="body"><b>Description</b></Typography></div>  
-                  <div><Typography variant="body">{toyListing.description}</Typography></div>
-                </div>
+            <Box sx={{ marginTop: "10px" }}>
+              <Typography variant="body">
+                <Avatar
+                  src="/AppLogo.png"
+                  alt="AppLogo"
+                  sx={{
+                    width: 30,
+                    height: 30,
+                    display: "inline-block",
+                    verticalAlign: "middle",
+                  }}
+                />
+                Joined <b>PlayItForward</b> in{" "}
+                {dateStringToMonthYear(toyGiver.create_date)}
+              </Typography>
             </Box>
-            <Box sx={{ padding: "20px 0" }}>
-
-                  {mapPosition.lat && <ToyMap lat={mapPosition.lat} lng={mapPosition.lng}/>}
-                  <Typography variant="body">{mapPosition.city}, {mapPosition.state}</Typography>
-            </Box>
-            <Divider/>
-            <Box sx={{ padding: "20px 0" }}>
-              <Typography variant="h6" sx={{ margin: "5px 0" }}>Posted by</Typography>
-                <Box className={styles.giverInformation}>
-                  <img src={toyGiver.profile_picture} alt="Toy giver profile picture" width="42px" height="42px" />
-                  <Typography variant="body" sx={{ marginLeft: "10px", lineHeight: "42px" }}>{toyGiver.nickname}</Typography>
-                </Box>
-                <Box sx={{ marginTop: "10px" }}>
-                  <Typography variant="body">
-                    <Avatar src="/AppLogo.png" alt="AppLogo" sx={{ width: 30, height: 30, display: "inline-block", verticalAlign: "middle" }}/>
-                     Joined <b>PlayItForward</b> in {dateStringToMonthYear(toyGiver.create_date)}
-                  </Typography>
-                </Box>
-            </Box>
-            <Divider/>
-            <Box sx={{ padding: "20px 0" }}>
-              <Typography variant="h6" sx={{ margin: "5px 0" }}>Send a message</Typography>
-              <TextField id="outlined-basic" onChange={handleMessageChange} value={newMessage} variant="outlined" sx={{ width: "100%" }} />
-              <br/>
-              <ActionButton linkTo="" text="&nbsp;Send" startIcon={<MailIcon/>} onClick={async () => await handleSendMessage()} fullWidth={true}/> 
-            </Box>
+          </Box>
+          <Divider />
+          <Box sx={{ padding: "20px 0" }}>
+            <Typography variant="h6" sx={{ margin: "5px 0" }}>
+              Send a message
+            </Typography>
+            <TextField
+              id="outlined-basic"
+              onChange={handleMessageChange}
+              value={newMessage}
+              variant="outlined"
+              sx={{ width: "100%" }}
+            />
+            <br />
+            <ActionButton
+              linkTo=""
+              text="&nbsp;Send"
+              startIcon={<MailIcon />}
+              onClick={async () => await handleSendMessage()}
+              fullWidth={true}
+            />
+          </Box>
         </Box>
       </Drawer>
-      <Box component="main" sx={{ flexGrow: 1, p: 3,  mt: 12 }}>
+      <Box component="main" sx={{ flexGrow: 1, p: 3, mt: 12 }}>
         <img src={toyListing.imageUrl} alt="Toy image" width="100%" />
       </Box>
     </Box>
@@ -262,7 +397,10 @@ const ListingDetail = () => {
 
 export function dateStringToMonthYear(dateString) {
   if (!dateString) return "";
-  return new Date(dateString).toLocaleDateString('en-US', { month: 'long', year: 'numeric' });
+  return new Date(dateString).toLocaleDateString("en-US", {
+    month: "long",
+    year: "numeric",
+  });
 }
 
-export default ListingDetail;   
+export default ListingDetail;
