12f61da075d8239b333db4d9c2fe14a44d2541c1
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2024-04-30 09:40:28 -0400

    Update IconMenu.jsx: replace Link import with react-router-dom's Link
    This commit updates the import statement for the Link component in IconMenu.jsx from "@mui/material" to "react-router-

--- src/components/UserProfile/IconMenu.jsx
+++ src/components/UserProfile/IconMenu.jsx
@@ -6,7 +6,6 @@ import {
   ListItem,
   Button,
   ListItemButton,
-  Link,
   Grid,
 } from "@mui/material";
 import Paper from "@mui/material/Paper";
@@ -23,6 +22,7 @@ import MailIcon from "@mui/icons-material/Mail";
 import Create from "../ToyList/Create";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import Drawer from "@mui/material/Drawer";
+import Link from "react-router-dom";
 
 const menuOptions = [
   { id: 0, text: "My Listings", icon: <ListIcon />, link: "/listings" },
