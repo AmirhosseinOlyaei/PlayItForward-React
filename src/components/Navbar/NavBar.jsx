31bba5e94475452e30a05902f8bc39a6c9c999f1
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2024-04-30 08:58:00 -0400

    "Add useLocation hook to set navbar style based on current route; useEffect to apply styles"

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,5 +1,6 @@
 // src/components/Navbar/NavBar.jsx
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import { Link as RouterLink, useNavigate } from "react-router-dom";
 import {
   AppBar,
@@ -27,10 +28,28 @@ import {
 import LettersAvatar from "../ListingDetail/LettersAvatar";
 
 const NavBar = ({ user }) => {
-  // if (!isVisible) return null;
+  const location = useLocation();
+  const [navbarStyle, setNavbarStyle] = useState({});
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      // Style for landing page
+      setNavbarStyle({
+        // backgroundColor: "blue", // Example: blue background on the landing page
+        // color: "white", // Example: white text color on the landing page
+        display: "none",
+      });
+    } else {
+      // Default style
+      setNavbarStyle({
+        // backgroundColor: "white", // Example: default background
+        // color: "black", // Example: default text color
+      });
+    }
+  }, [location]);
 
   const handleMenuClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -51,7 +70,11 @@ const NavBar = ({ user }) => {
   };
 
   return (
-    <AppBar component="nav" sx={{ bgcolor: "rgba(33, 150, 253, 0.8)" }}>
+    <AppBar
+      component="nav"
+      sx={{ bgcolor: "rgba(33, 150, 253, 0.8)" }}
+      style={navbarStyle}
+    >
       <Toolbar
         sx={{
           display: "flex",
